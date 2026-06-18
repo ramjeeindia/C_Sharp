94dc3c0d145ed01e7905f18ed6a657a09dbe06fd
Ramjee Singh <ramjee.india@gmail.com>
2026-06-18 21:51:34 +0530

    sql inj

---
+++ ADO_NET/ADO/ADO/4_SqlCommand/insertnonquery.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO._4_SqlCommand
+{
+    public partial class insertnonquery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                //SqlCommand cmd = new SqlCommand();
+                //cmd.CommandText = ("INSERT INTO [dbo].[tblCities] VALUES('PATNA', 1)");
+                //cmd.Connection = con;
+                //con.Open();
+                //int TotalRowAffected = cmd.ExecuteNonQuery();
+                //Response.Write("The Total Row Affected is : " + TotalRowAffected.ToString());
+
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                con.Open();
+
+                cmd.CommandText = ("DELETE FROM [dbo].[tblCities] WHERE CityId = 29");
+                int TotalRowDel = cmd.ExecuteNonQuery();
+                Response.Write("The Total Row Deleted is : " + TotalRowDel.ToString() + "</br>");
+
+                cmd.CommandText = ("INSERT INTO [dbo].[tblCities] VALUES('PATNA', 1)");
+                int TotalRowAffected = cmd.ExecuteNonQuery();
+                Response.Write("The Total Row Inserted is : " + TotalRowAffected.ToString() + "</br>");
+
+                cmd.CommandText = ("UPDATE [dbo].[tblCities] SET CityName = 'Rajgir' WHERE CityId=25");
+                int TotalRowUpdated = cmd.ExecuteNonQuery();
+                Response.Write("The Total Row Updated is : " + TotalRowUpdated.ToString() + "</br>");
+
+            }
+        }
+    }
+}
---
+++ ADO_NET/ADO/ADO/5_SqlInjection/InjPrevention.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO._5_SqlInjection
+{
+    public partial class InjPrevention : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btngetitm_Click(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                //SqlCommand cmd = new SqlCommand("SELECT * FROM tblProductInventory WHERE ProductName Like @ProdName", con);
+                //cmd.Parameters.AddWithValue("@ProdName",TextBox1.Text + "%");
+                ////this method required two value for parameter and % for like command
+                //con.Open();
+                //GridView1.DataSource = cmd.ExecuteReader();
+                //GridView1.DataBind();
+
+
+                //SqlCommand cmd = new SqlCommand("SELECT ItemCode , ItemName, OnHand FROM OITM WHERE ItemName Like @ItemName", con);
+                //cmd.Parameters.AddWithValue("@ItemName", TextBox1.Text + "%");
+                ////this method required two value for parameter and % for like command
+                //con.Open();
+                //GridView1.DataSource = cmd.ExecuteReader();
+                //GridView1.DataBind();
+
+
+
+                // link with sql store procedure
+                SqlCommand cmd = new SqlCommand("spGetItemName", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure; // this method call store proc
+                cmd.Parameters.AddWithValue("@ItemName", TextBox1.Text + "%");
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
