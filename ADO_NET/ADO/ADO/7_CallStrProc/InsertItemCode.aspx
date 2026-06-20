ec50d976e141c4ed4d0884f76d03fffe63c65fc5
Ramjee Singh <ramjee.india@gmail.com>
2026-06-20 21:24:53 +0530

    data reader

---
+++ ADO_NET/ADO/ADO/7_CallStrProc/InsertItemCode.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InsertItemCode.aspx.cs" Inherits="ADO._7_CallStrProc.InsertItemCode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ADO_NET/ADO/ADO/8_SqlDataRdr/SqlDataRdr.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO._8_SqlDataRdr
+{
+    public partial class SqlDataRdr : System.Web.UI.Page 
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open(); // need to open before reader
+                SqlCommand cmd = new SqlCommand ("SELECT ITEMCODE, ITEMNAME, ONHAND FROM OITM", con);
+
+               // SqlDataReader rdr = new SqlDataReader(); WE CAN NOT USE 
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource =	rdr;
+                GridView1.DataBind ();
+
+            }
+
+        }
+
+    }
+}
