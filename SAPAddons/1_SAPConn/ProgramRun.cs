e022393c7900cb4c4dc4acafbfab0b4913e364f1
Ramjee Singh <ramjee.india@gmail.com>
2026-07-26 22:43:52 +0530

    sapconn

---
+++ SAPAddons/1_SAPConn/ProgramRun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SAPConn
+{
+    class ProgramRun
+    {
+        [STAThread]
+        static void Main()
+        {
+            B1Connection objB1Connection = new B1Connection();
+        }
+    }
+}
