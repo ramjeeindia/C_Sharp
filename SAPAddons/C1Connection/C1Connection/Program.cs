dee3ab3bbff0d92d53b3482575e27e7efc56c81a
Ramjee Singh <ramjee.india@gmail.com>
2026-08-04 21:03:24 +0530

    coonection method 2

---
+++ SAPAddons/C1Connection/C1Connection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C1Connection
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            UIConnection uiConnection = new UIConnection();
+        }
+    }
+}
---
+++ SAPAddons/C1Connection/C1Connection/UIConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAPbouiCOM;
+
+
+namespace C1Connection
+{
+    class UIConnection
+    {
+        public Application SBO_Application { get; private set; }
+
+        
+
+        public UIConnection()
+        {
+            Connect();
+            SBO_Application.MessageBox("Connected to SAP Business One UI API successfully.");
+        }
+
+        private void Connect()
+        {
+            SboGuiApi guiApi = new SboGuiApi();
+            string connStr = Environment.GetCommandLineArgs()[1];
+
+            guiApi.Connect(connStr);
+            SBO_Application = guiApi.GetApplication(-1);
+
+            
+        }
+    }
+
+}
