87e836cb2a6d21dc7bc1b5f18734c70dd7a49822
Ramjee Singh <ramjee.india@gmail.com>
2026-08-04 20:33:03 +0530

    sap connection working

---
+++ SAPAddons/B1Connection/B1Connection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B1Connection
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            UIConn connection = new UIConn();  // 1st method      
+        }
+    }
+}
---
+++ SAPAddons/B1Connection/B1Connection/UIConn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAPbouiCOM;
+
+namespace B1Connection
+{
+    class UIConn
+    {
+        private SAPbouiCOM.Application SBO_Application;
+        
+        public UIConn()
+        {
+            SetApplication();
+            SBO_Application.MessageBox("Hello, World!");
+        }
+
+        private void SetApplication()
+        {
+            string connectionString = Environment.GetCommandLineArgs().GetValue(1).ToString();
+            SboGuiApi sboGuiApi = new SboGuiApi();
+            sboGuiApi.Connect(connectionString);
+            SBO_Application = sboGuiApi.GetApplication(-1);
+        }
+    }
+}
