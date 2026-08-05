f3e08b991020bf5545feceb783abb15ffa0a582b
Ramjee Singh <ramjee.india@gmail.com>
2026-08-05 21:29:10 +0530

    connection method3

---
+++ SAPAddons/D1Connection/D1Connection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D1Connection
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Connection connection = new Connection();  // Creating object of Connection class
+        }
+    }
+}
